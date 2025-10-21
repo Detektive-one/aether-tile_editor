c3b344f2cd06f30640da7dfed21a5ce1f570eae6
Detektive-one <kartikey20csu050@ncuindia.edu>
2025-10-21 23:34:23 +0530

    commit: initial project setup

---
+++ main.py
@@ -0,0 +1,15 @@
+ #!/usr/bin/env python3
+import sys
+from PySide6.QtWidgets import QApplication
+from ui.main_window import TileEditorMainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    app.setApplicationName("Aether Tile Editor")
+    app.setOrganizationName("Aether Framework")
+    window = TileEditorMainWindow()
+    window.show()
+    sys.exit(app.exec())
+if __name__ == "__main__":
+    main()
